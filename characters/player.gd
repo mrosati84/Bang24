dad907a29d3c02d769a8b34f7b49b564e2b087f1
Matteo Rosati <rosati.matteo@gmail.com>
2023-09-22 13:13:16 +0200

    smooth movements

--- characters/player.gd
+++ characters/player.gd
@@ -1,7 +1,9 @@
 extends CharacterBody2D
 
 @export var speed = 150
-@export var rotation_smoothing = 0.02
+@export var body_rotation_smoothing = 0.02
+@export var turret_rotation_smoothing = 0.05
+@export var acceleration = 0.1
 
 # oggetto di riferimento che punta sempre davanti al carro
 # e viene usato per dare la direzione di movimento in avanti
@@ -16,30 +18,30 @@ func _physics_process(_delta):
 	var body_rotation = Input.get_axis("left", "right")
 	
 	if body_rotation != 0:
-		rotate(body_rotation * rotation_smoothing)
+		rotate(body_rotation * body_rotation_smoothing)
 		
 		# preserva la rotazione della torretta quando ruota anche il carro
 		if last_mouse_position != Vector2.ZERO:
-			rotate_turret(last_mouse_position)
+			rotate_turret()
 		if not body.is_playing():
 			body.play("walk")
 	else:
 		body.stop()
 	
 	# gestisce movimento avanti/indietro
-	velocity = position.direction_to(forward_marker.get_global_position()) * speed * Input.get_axis("down", "up")
+	velocity = velocity.lerp(position.direction_to(forward_marker.get_global_position()) * speed * Input.get_axis("down", "up"), acceleration)
 	
+	rotate_turret()
 	move_and_slide()
 
 func _process(_delta):
 	if Input.is_action_just_pressed("fire"):
 		turret.fire()
 
-func _input(event):
-	if event is InputEventMouse:
-		last_mouse_position = event.position
-		rotate_turret(last_mouse_position)
-
-func rotate_turret(mouse_position: Vector2):
-	turret.look_at(mouse_position)
-	turret.rotate(PI/2)
+func rotate_turret():
+	# usa lerp_angle per una rotazione morbida
+	var v = get_global_mouse_position() - turret.global_position
+	var angle = v.angle()
+	var r = turret.global_rotation
+	
+	turret.global_rotation = lerp_angle(r, angle + PI/2, turret_rotation_smoothing)
