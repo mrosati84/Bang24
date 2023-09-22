b4bc119bc741120f1619c00f41c355639b672bb0
Matteo Rosati <rosati.matteo@gmail.com>
2023-09-22 11:56:12 +0200

    minor refactor

--- characters/player.gd
+++ characters/player.gd
@@ -3,6 +3,12 @@ extends CharacterBody2D
 @export var speed = 150
 @export var rotation_smoothing = 0.02
 
+# oggetto di riferimento che punta sempre davanti al carro
+# e viene usato per dare la direzione di movimento in avanti
+@onready var forward_marker : Marker2D = $Body/Marker2D
+@onready var body : AnimatedSprite2D = $Body
+@onready var turret : AnimatedSprite2D = $Turret
+
 var last_mouse_position : Vector2 = Vector2.ZERO
 
 func _physics_process(_delta):
@@ -15,19 +21,19 @@ func _physics_process(_delta):
 		# preserva la rotazione della torretta quando ruota anche il carro
 		if last_mouse_position != Vector2.ZERO:
 			rotate_turret(last_mouse_position)
-		if not $Body.is_playing():
-			$Body.play("walk")
+		if not body.is_playing():
+			body.play("walk")
 	else:
-		$Body.stop()
+		body.stop()
 	
 	# gestisce movimento avanti/indietro
-	velocity = position.direction_to($Body/Marker2D.get_global_position()) * speed * Input.get_axis("down", "up")
+	velocity = position.direction_to(forward_marker.get_global_position()) * speed * Input.get_axis("down", "up")
 	
 	move_and_slide()
 
 func _process(_delta):
 	if Input.is_action_just_pressed("fire"):
-		$Turret.fire()
+		turret.fire()
 
 func _input(event):
 	if event is InputEventMouse:
@@ -35,5 +41,5 @@ func _input(event):
 		rotate_turret(last_mouse_position)
 
 func rotate_turret(mouse_position: Vector2):
-	$Turret.look_at(mouse_position)
-	$Turret.rotate(PI/2)
+	turret.look_at(mouse_position)
+	turret.rotate(PI/2)
