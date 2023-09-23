3378d650d2c4b6d166729a1163607da73f2b7f20
Matteo Rosati <rosati.matteo@gmail.com>
2023-09-24 01:48:50 +0200

    refactor bullet

--- characters/Bullet.gd
+++ characters/Bullet.gd
@@ -1,17 +1,12 @@
-extends CharacterBody2D
+extends Area2D
 
-const SPEED = 500.0
+@export var speed : float = 1.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var direction : Vector2
 
 func _ready():
 	direction = Vector2(0,-1).rotated(rotation)
+	print(direction)
 
-func _physics_process(delta):
-	# Add the gravity.
-	velocity = SPEED * direction
-	if not is_on_floor():
-		velocity.y += gravity * 1 * delta
-	move_and_slide()
+func _physics_process(_delta):
+	position = position + direction * speed
--- characters/turret.gd
+++ characters/turret.gd
@@ -1,12 +1,17 @@
 extends AnimatedSprite2D
 
-@export var bullet :PackedScene
+@export var bullet : PackedScene
+
+
+func _process(_delta):
+	if Input.is_action_just_pressed("fire"):
+		fire(global_position, global_rotation)
+
 
 func fire(pos, rot):
 	# cooldown che dura quanto l'animazione
 	if not is_playing():
 		play("fire")
-		
 		var b = bullet.instantiate()
 		#@TODO: retrieve fields from $Turret instead of passing them
 		b.global_position = pos
