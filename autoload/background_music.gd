2269623b4d525e21c2adb76a76d8e3397dabb060
Matteo Rosati <rosati.matteo@gmail.com>
2023-10-09 00:36:21 +0200

    fix warnings

--- autoload/background_music.gd
+++ autoload/background_music.gd
@@ -7,12 +7,6 @@ func _ready():
 	$"/root/BackgroundMusic".connect("finished", Callable(self,"_on_loop_sound").bind($"/root/BackgroundMusic"))
 	$"/root/BackgroundMusic".play()
 
-func _on_loop_sound(player):
+func _on_loop_sound(_player):
 	$"/root/BackgroundMusic".stream_paused = false
 	$"/root/BackgroundMusic".play()
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
